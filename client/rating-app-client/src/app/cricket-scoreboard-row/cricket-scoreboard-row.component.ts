3e199c6d44e56ef6532c130af5df84debe30bd73
alinden <alinden@raybeam.com>
2018-12-05 13:55:16 -0500

    enter bull and enter miss badly formatted

--- client/rating-app-client/src/app/cricket-scoreboard-row/cricket-scoreboard-row.component.ts
+++ client/rating-app-client/src/app/cricket-scoreboard-row/cricket-scoreboard-row.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { SimpleChanges, Component, OnInit, Input, OnChanges } from '@angular/core';
 
-import { C } from '../cricket-state';
+import { C, CricketState } from '../cricket-state';
 
 @Component({
   selector: 'app-cricket-scoreboard-row',
   templateUrl: './cricket-scoreboard-row.component.html',
   styleUrls: ['./cricket-scoreboard-row.component.css']
 })
-export class CricketScoreboardRowComponent implements OnInit {
+export class CricketScoreboardRowComponent implements OnChanges {
   @Input() scores: number[];
   @Input() shots: C[];
+  @Input() turnIndex: number;
+  activePlayer = 0;
+  leftActive = true;
+  rightActive = false;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('onChanges');
+    const newActivePlayer = this.turnIndex;
+    if (newActivePlayer !== this.activePlayer) {
+      this.activePlayer = newActivePlayer;
+      if (newActivePlayer === 0) {
+        console.log('setting left to active');
+        this.leftActive = true;
+        this.rightActive = false;
+      } else {
+        console.log('setting right to active');
+        this.leftActive = true;
+        this.rightActive = false;
+      }
+    }
   }
 
 }
