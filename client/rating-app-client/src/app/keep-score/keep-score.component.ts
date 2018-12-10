38a082db8b98787d18a47e6ced313d302901109d
alinden <alinden@raybeam.com>
2018-12-10 01:04:44 -0500

    undo works

--- client/rating-app-client/src/app/keep-score/keep-score.component.ts
+++ client/rating-app-client/src/app/keep-score/keep-score.component.ts
@@ -25,6 +25,7 @@ export class KeepScoreComponent implements OnInit {
   league: WithId<League>;
   rules: string;
   mode: string;
+  shotHistory: DartShot[] = [];
 
   private sub: any;
 
@@ -37,19 +38,67 @@ export class KeepScoreComponent implements OnInit {
   }
 
   recordLag() {
-    console.log('recordLag');
-    console.log('this.playerOneId');
-    console.log(this.playerOneId);
-    console.log('this.playerTwoId');
-    console.log(this.playerTwoId);
     const playerOne = this.client.getRatedUserById(this.playerOneId, this.leagueId);
     const playerTwo = this.client.getRatedUserById(this.playerTwoId, this.leagueId);
-    console.log('JSON.stringify(this.playerOne)');
-    console.log(JSON.stringify(playerOne));
-    console.log('JSON.stringify(this.playerTwo)');
-    console.log(JSON.stringify(playerTwo));
     this.startGame([playerOne, playerTwo]);
     this.enterGameMode();
+  }
+
+  nextTurn() {
+    const shotsSkipped = 3 - this.liveGame.state.shotsRemaining;
+    this.liveGame.state.shotsRemaining = 3;
+    this.liveGame.state.turnIndex = (this.liveGame.state.turnIndex + 1) % 2;
+    this.shotHistory.push({ value: -1, count: shotsSkipped });
+    console.log(this.liveGame.state);
+  }
+
+  undo() {
+    const prevShot = this.shotHistory.pop();
+    const prevShotsRemaining = this.liveGame.state.shotsRemaining;
+    const prevTurnIndex = this.liveGame.state.turnIndex;
+    let newTurnIndex;
+    let newShotsRemaining;
+    if (prevShot.value === -1) {
+      // prevShot was a "Next Turn", that skipped prevShot.count shots.
+      newShotsRemaining = 3 - prevShot.count;
+      newTurnIndex = (prevTurnIndex + 1) % 2;
+    } else {
+      // prevShot was not a "Next Turn".
+      if (prevShotsRemaining === 3) {
+        // Undoing at start of new turn - rewind to last shot of previous turn.
+        newShotsRemaining = 1;
+        newTurnIndex = (prevTurnIndex + 1) % 2;
+      } else {
+        // Undoing mid-turn - add one to shot counter.
+        newShotsRemaining = prevShotsRemaining + 1;
+        newTurnIndex = prevTurnIndex;
+      }
+      const [marks, scores] = this.getMarksAndScores(prevShot);
+      const marksBefore = marks[newTurnIndex];
+      const scoresBefore = scores[newTurnIndex];
+      let marksAfter = marksBefore;
+      let scoresAfter = scoresBefore;
+      if (marksBefore === 3) {
+        // Player had closed that number - undo points first.
+        const numberOfScores = scoresBefore / prevShot.value;
+        if (numberOfScores > prevShot.count) {
+          // Player has points in that number from before the dart being undone.
+          scoresAfter = (numberOfScores - prevShot.count) * prevShot.value;
+        } else {
+          // Player only scored on this number in the dart begin undone.
+          scoresAfter = 0;
+          marksAfter = 3 - (prevShot.count - numberOfScores);
+        }
+      } else {
+        // Player has not closed that number.
+        marksAfter = marksBefore - prevShot.count;
+      }
+      this.updateMarksAndScores(prevShot, newTurnIndex, marksAfter, scoresAfter);
+    }
+    // Update game state.
+    this.liveGame.isOver = false;
+    this.liveGame.state.shotsRemaining = newShotsRemaining;
+    this.liveGame.state.turnIndex = newTurnIndex;
   }
 
   saveGame() {
@@ -61,92 +110,122 @@ export class KeepScoreComponent implements OnInit {
     this.router.navigate(['/games']);
   }
 
-  cancelGame() {
-    console.log('cance');
+  updateMarksAndScores(shot: DartShot, turnIndex: number, marksAfter: number, scoresAfter: number) {
+    switch (shot.value) {
+      case 20: {
+        this.liveGame.state.twenties[turnIndex] = marksAfter;
+        this.liveGame.state.twentyScores[turnIndex] = scoresAfter;
+        break;
+      }
+      case 19: {
+        this.liveGame.state.nineteens[turnIndex] = marksAfter;
+        this.liveGame.state.nineteenScores[turnIndex] = scoresAfter;
+        break;
+      }
+      case 18: {
+        this.liveGame.state.eightteens[turnIndex] = marksAfter;
+        this.liveGame.state.eightteenScores[turnIndex] = scoresAfter;
+        break;
+      }
+      case 17: {
+        this.liveGame.state.seventeens[turnIndex] = marksAfter;
+        this.liveGame.state.seventeenScores[turnIndex] = scoresAfter;
+        break;
+      }
+      case 16: {
+        this.liveGame.state.sixteens[turnIndex] = marksAfter;
+        this.liveGame.state.sixteenScores[turnIndex] = scoresAfter;
+        break;
+      }
+      case 15: {
+        this.liveGame.state.fifteens[turnIndex] = marksAfter;
+        this.liveGame.state.fifteenScores[turnIndex] = scoresAfter;
+        break;
+      }
+      case 25: {
+        this.liveGame.state.bullseyes[turnIndex] = marksAfter;
+        this.liveGame.state.bullseyeScores[turnIndex] = scoresAfter;
+        break;
+      }
+    }
+    this.liveGame.state.scores[turnIndex] =
+      this.liveGame.state.twentyScores[turnIndex] +
+      this.liveGame.state.nineteenScores[turnIndex] +
+      this.liveGame.state.eightteenScores[turnIndex] +
+      this.liveGame.state.seventeenScores[turnIndex] +
+      this.liveGame.state.sixteenScores[turnIndex] +
+      this.liveGame.state.fifteenScores[turnIndex] +
+      this.liveGame.state.bullseyeScores[turnIndex];
+  }
+
+  getMarksAndScores(shot: DartShot) {
+    let marks: C[];
+    let scores: number[];
+    let miss = false;
+    switch (shot.value) {
+      case 20: {
+        marks = this.liveGame.state.twenties;
+        scores = this.liveGame.state.twentyScores;
+        break;
+      }
+      case 19: {
+        marks = this.liveGame.state.nineteens;
+        scores = this.liveGame.state.nineteenScores;
+        break;
+      }
+      case 18: {
+        marks = this.liveGame.state.eightteens;
+        scores = this.liveGame.state.eightteenScores;
+        break;
+      }
+      case 17: {
+        marks = this.liveGame.state.seventeens;
+        scores = this.liveGame.state.seventeenScores;
+        break;
+      }
+      case 16: {
+        marks = this.liveGame.state.sixteens;
+        scores = this.liveGame.state.sixteenScores;
+        break;
+      }
+      case 15: {
+        marks = this.liveGame.state.fifteens;
+        scores = this.liveGame.state.fifteenScores;
+        break;
+      }
+      case 25: {
+        marks = this.liveGame.state.bullseyes;
+        scores = this.liveGame.state.bullseyeScores;
+        break;
+      }
+      default: {
+        miss = true;
+      }
+    }
+    return [marks, scores];
   }
 
   recordShot(shot: DartShot) {
     if (!this.liveGame.isOver) {
-      console.log('recordShot');
-      console.log(JSON.stringify(shot));
 
       // get relevant data from game state
-      let shots: C[];
-      let scores: number[];
-      let miss = false;
-      switch (shot.value) {
-        case 20: {
-          shots = this.liveGame.state.twenties;
-          scores = this.liveGame.state.twentyScores;
-          break;
-        }
-        case 19: {
-          shots = this.liveGame.state.nineteens;
-          scores = this.liveGame.state.nineteenScores;
-          break;
-        }
-        case 18: {
-          shots = this.liveGame.state.eightteens;
-          scores = this.liveGame.state.eightteenScores;
-          break;
-        }
-        case 17: {
-          shots = this.liveGame.state.seventeens;
-          scores = this.liveGame.state.seventeenScores;
-          break;
-        }
-        case 16: {
-          shots = this.liveGame.state.sixteens;
-          scores = this.liveGame.state.sixteenScores;
-          break;
-        }
-        case 15: {
-          shots = this.liveGame.state.fifteens;
-          scores = this.liveGame.state.fifteenScores;
-          break;
-        }
-        case 25: {
-          shots = this.liveGame.state.bullseyes;
-          scores = this.liveGame.state.bullseyeScores;
-          break;
-        }
-        default: {
-          miss = true;
-        }
-      }
+      const [marks, scores] = this.getMarksAndScores(shot);
 
-      // record closing/ points
-      if (!miss) {
-        console.log('not a miss');
-        const playerShotsBefore: number = shots[this.liveGame.state.turnIndex];
-        console.log('playerShots before');
-        console.log(JSON.stringify(playerShotsBefore));
-        const playerShotsAfter = Math.min(3, playerShotsBefore + shot.count);
-        console.log('playerShots after');
-        console.log(JSON.stringify(playerShotsAfter));
-        shots[this.liveGame.state.turnIndex] = playerShotsAfter;
-        const shotsUsed = playerShotsAfter - playerShotsBefore;
-        console.log('shotsUsed');
-        console.log(shotsUsed);
-        const remainingCount = shot.count - shotsUsed;
-        console.log('remainingCount');
-        console.log(remainingCount);
-        const shotScore = shot.value * remainingCount;
-        if (playerShotsAfter === C.Closed) {
-          console.log('player shots closed - recording possible points');
-          if (this.liveGame.players.length === 2) {
-            // add score to player if other players hasn't closed
-            const otherPlayerIndex = (this.liveGame.state.turnIndex + 1) % 2;
-            const otherPlayerClosed = (shots[otherPlayerIndex] === C.Closed);
-            if (!otherPlayerClosed) {
-              scores[this.liveGame.state.turnIndex] += shotScore;
-              this.liveGame.state.scores[this.liveGame.state.turnIndex] += shotScore;
-            }
-          } else {
-            console.log('multiplayer cricket scoring not implemented yet');
+      const playerMarksBefore: number = marks[this.liveGame.state.turnIndex];
+      const playerMarksAfter = Math.min(3, playerMarksBefore + shot.count);
+      marks[this.liveGame.state.turnIndex] = playerMarksAfter;
+      const marksUsed = playerMarksAfter - playerMarksBefore;
+      const remainingCount = shot.count - marksUsed;
+      const shotScore = shot.value * remainingCount;
+      if (playerMarksAfter === C.Closed) {
+        if (this.liveGame.players.length === 2) {
+          // add score to player if other players hasn't closed
+          const otherPlayerIndex = (this.liveGame.state.turnIndex + 1) % 2;
+          const otherPlayerClosed = (marks[otherPlayerIndex] === C.Closed);
+          if (!otherPlayerClosed) {
+            scores[this.liveGame.state.turnIndex] += shotScore;
+            this.liveGame.state.scores[this.liveGame.state.turnIndex] += shotScore;
           }
-        } else {
-          console.log('player shots not closed yet');
         }
       }
 
@@ -181,6 +260,8 @@ export class KeepScoreComponent implements OnInit {
       } else {
         this.liveGame.state.shotsRemaining = this.liveGame.state.shotsRemaining - 1;
       }
+
+      this.shotHistory.push(shot);
     }
   }
 
@@ -226,15 +307,9 @@ export class KeepScoreComponent implements OnInit {
     });
     if (!this.client.initialized) {
       this.client.loadAllData();
-      console.log('client loaded all data');
       this.league = this.client.getLeagueById(this.leagueId);
-      console.log('found league');
-      console.log('JSON.stringify(this.league)');
-      console.log(JSON.stringify(this.league));
     } else {
-      console.log('client already loaded all data');
       this.league = this.client.getLeagueById(this.leagueId);
-      console.log('found league');
     }
     this.enterLagMode();
   }
