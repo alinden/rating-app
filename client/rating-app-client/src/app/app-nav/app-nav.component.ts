7ced4e48b3e6d406cc6deba50ddde19b7609fd38
Alex Linden <alexwlinden@gmail.com>
2019-03-02 20:24:51 +0000

    added fields

--- client/rating-app-client/src/app/app-nav/app-nav.component.ts
+++ client/rating-app-client/src/app/app-nav/app-nav.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { LeagueService } from '../league.service';
+import { WithId } from '../with-id';
+import { League } from '../league';
 
 @Component({
   selector: 'app-app-nav',
@@ -9,12 +12,31 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app-nav.component.css'],
 })
 export class AppNavComponent {
+  leagues: WithId<League>[] = [];
+  views: string[] = [
+    'Standings',
+    'Months',
+    'Ratings',
+    'Recent Games',
+  ];
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private leagueService: LeagueService,
+  ) {}
+
+  ngOnInit() {
+    this.leagueService.getLeagues().subscribe(leagues => {
+      console.log('app-nav ngOnInit');
+      console.log('leagues');
+      console.log(leagues);
+      this.leagues = leagues;
+    });
+  }
 
 }
